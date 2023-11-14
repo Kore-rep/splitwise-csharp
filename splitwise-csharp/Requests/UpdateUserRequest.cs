4f2196112bb03945e1b92cc3bec97cd33a33feb2
Angus Longmore <anguslongmora@gmail.com>
2023-11-14 14:44:58 +0200

    Add Update User endpoint

---
+++ splitwise-csharp/Requests/UpdateUserRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SplitwiseCSharp.Requests;
+
+/// <summary>
+/// A request body to update a user. Null values will not be sent in the request.
+/// </summary>
+public class UpdateUserRequest
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Email { get; set; }
+    public string? Password { get; set; }
+    public string? Locale { get; set; }
+    public string? DefaultCurrency { get; set; }
+}
