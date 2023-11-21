d1594fc05f1d727fc44329af9b9395da05dd2761
Angus Longmore <anguslongmora@gmail.com>
2023-11-21 22:44:51 +0200

    Rename solution and add Tests

---
+++ SplitwiseDotnetTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SplitwiseDotnetTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
